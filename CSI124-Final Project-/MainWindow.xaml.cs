f3c26f7cd5f7ad82fdeb1d04d8d56d8b25449264
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-06-18 19:51:31 -0700

    updated

--- CSI124-Final Project-/MainWindow.xaml.cs
+++ CSI124-Final Project-/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,10 +9,13 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FinalProjectClassLibrary;
+
 
 namespace CSI124_Final_Project_
 {
@@ -20,9 +24,54 @@ namespace CSI124_Final_Project_
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        List<Account> _members = new List<Account>();
         public MainWindow()
         {
             InitializeComponent();
+            Preload();
+            lbDebug.ItemsSource = _members;   
+        }
+
+        public void Preload()
+        {
+            _members.Add(new Account("Meili", "Zheng", "meilizheng@gmail.com", "meili"));
+            _members.Add(new Account("Belle", "Miao", "bellemaio@gmail.com", "belle"));
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string email = textEmail.Text;
+            string password = TextPassword.Text;
+
+            if(IsValidUser(email, password))
+            {
+                new Workout().Show();
+            }
+            else
+            {
+                MessageBox.Show("Please entered a valid information");
+            }
+        }
+
+
+        public bool IsValidUser(string email, string password)
+        {
+            foreach (var account in _members)
+            {
+                if(email == account.Email && password == account.Password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void lbDebug_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Account selected = (Account)lbDebug.SelectedItem;
+            textEmail.Text = selected.Email;
+            TextPassword.Text = selected?.Password;
         }
     }
 }
---
+++ CSI124-Final Project-/Workout.xaml.cs
@@ -0,0 +1,38 @@
+﻿using FinalProjectClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSI124_Final_Project_
+{
+    /// <summary>
+    /// Interaction logic for Workout.xaml
+    /// </summary>
+    public partial class Workout : Window
+    {
+        List<Exercies> _exercise = new List<Exercies> ();
+        public Workout()
+        {
+            InitializeComponent();
+            Preload();
+            lvExercises.ItemsSource = _exercise;
+
+        }
+
+        public void Preload()
+        {
+            _exercise.Add(new Exercies("Run", 1000));
+            _exercise.Add(new Exercies("Weights", 400));
+        }
+    }
+}
